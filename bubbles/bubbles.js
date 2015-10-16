4c75bf0b7f5b93fa7026022917b40ffe294c54c9
Michael Thornbrugh <mpthornbrugh@gmail.com>
2015-10-15 21:26:17 -0500

    Updated the bubble view to angularjs

--- bubbles/bubbles.js
+++ bubbles/bubbles.js
@@ -1,16 +1,41 @@
-window.onload = window.onresize = function () {
-	var container = document.getElementById("bubble-container");
+'use strict';
 
-	var body = document.body,
-		html = document.documentElement;
+angular.module('bubbleView.view', ['ngRoute'])
 
-	var height = Math.max( body.scrollHeight, body.offsetHeight,
-		html.clientHeight, html.scrollHeight, html.offsetHeight );
+	.config(['$routeProvider', function ($routeProvider) {
+		$routeProvider.when('/view', {
+			templateUrl: 'view.html',
+			controller: 'ViewCtrl'
+		});
+	}])
 
-	var neededFontSize = Math.floor(height/37);
+	.controller('ViewCtrl', ['$scope', function ($scope) {
+		//Calculate the font-size for the view
+		var container = document.getElementById("bubble-container");
+		var body = document.body,
+			html = document.documentElement;
 
-	container.setAttribute("style", "font-size:" + neededFontSize + "px;")
-};
+		var height = Math.max( body.scrollHeight, body.offsetHeight,
+			html.clientHeight, html.scrollHeight, html.offsetHeight );
+
+		var neededFontSize = Math.floor(height/37);
+		container.setAttribute("style", "font-size:" + neededFontSize + "px;")
+
+		window.onresize = function () {
+			height = Math.max( body.scrollHeight, body.offsetHeight,
+				html.clientHeight, html.scrollHeight, html.offsetHeight );
+
+			var neededFontSize = Math.floor(height/37);
+
+			container.setAttribute("style", "font-size:" + neededFontSize + "px;")
+		};
+
+		$scope.poke = function () {
+			alert("Poke!");
+		};
+
+
 
 //In order to scale the bubble view you can mess with the font-size style on the bubble-container div. Default is 16px
-//In order to calculate the right font-size: take the current height and divide by 37 (number of em's from top to bottom with all circles shown.)
+//In order to calculate the right font-size: take the current height and divide by 37 (number of em's from top to bottom with all circles shown.
+	}]);
