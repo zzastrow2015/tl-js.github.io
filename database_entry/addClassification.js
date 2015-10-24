ea1237aeeb0395252365a9a6c31f4f4de1fe378b
Michael Thornbrugh <mpthornbrugh@gmail.com>
2015-10-24 12:08:26 -0500

    Adjusted the database entry pages

--- database_entry/addClassification.js
+++ database_entry/addClassification.js
@@ -28,7 +28,102 @@ angular.module('databaseEntry.add', ['ngRoute'])
 		};
 
 		$scope.fontAwesomeIcons = [
+			'fa-automobile',
+			'fa-bank',
+			'fa-birthday-cake',
+			'fa-book',
+			'fa-briefcase',
+			'fa-building',
+			'fa-building-o',
+			'fa-cab',
+			'fa-calculator',
+			'fa-calendar',
+			'fa-calendar-check-o',
+			'fa-calendar-minus-o',
+			'fa-calendar-o',
+			'fa-calendar-plus-o',
+			'fa-calendar-times-o',
+			'fa-camera',
+			'fa-car',
+			'fa-cog',
+			'fa-comment',
+			'fa-comment-o',
+			'fa-commenting',
+			'fa-commenting-o',
+			'fa-comments',
+			'fa-comments-o',
+			'fa-compass',
+			'fa-desktop',
+			'fa-envelope',
+			'fa-envelope-o',
+			'fa-eye',
+			'fa-female',
+			'fa-flag',
+			'fa-flag-checkered',
+			'fa-flag-o',
+			'fa-globe',
+			'fa-graduation-cap',
+			'fa-home',
+			'fa-industry',
+			'fa-legal',
 			'fa-lightbulb-o',
-			'fa-photo'
+			'fa-lock',
+			'fa-magic',
+			'fa-male',
+			'fa-map',
+			'fa-map-marker',
+			'fa-map-o',
+			'fa-map-pin',
+			'fa-map-signs',
+			'fa-microphone',
+			'fa-music',
+			'fa-paw',
+			'fa-phone',
+			'fa-photo',
+			'fa-question',
+			'fa-television',
+			'fa-trademark',
+			'fa-trophy',
+			'fa-university',
+			'fa-unlock',
+			'fa-unlock-alt',
+			'fa-user',
+			'fa-users',
+			'fa-warning',
+			'fa-youtube-play'
 		];
+
+		$scope.isDropdown = false;
+
+		$scope.fontAwesomeIcons = $scope.fontAwesomeIcons.sort();
+
+		$scope.selectIcon = function(icon) {
+			$scope.selectedIcon = icon;
+		}
 	}]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
