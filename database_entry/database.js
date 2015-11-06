16ed7f5b0fcc9cf451303c9dfcf47fc2f176af0d
Michael Thornbrugh <mpthornbrugh@gmail.com>
2015-11-06 00:54:49 -0600

    Made it so that the form actually adds to a database now. Also the get button will do a console log of every item that we have. Need to create a new view to delete items

--- database_entry/database.js
+++ database_entry/database.js
@@ -9,15 +9,56 @@ angular.module('databaseEntry.view', ['ngRoute'])
 		});
 	}])
 
-	.controller('ViewCtrl', ['$scope', '$location', function ($scope, $location) {
-		var classifications = [
-			{
-				"title": "Invention",
-				"icon": "fa-lightbulb-o"
-			}
-		];
+	.controller('ViewCtrl', ['$scope', '$location', '$http', function ($scope, $location, $http) {
+		var apiUrl = "https://historicaldv.herokuapp.com/";
 
-		$scope.classifications = classifications;
+		$scope.get = function () {
+			$http.get(apiUrl + "getItems").
+				success(function (data) {
+					console.log("Results: " + JSON.stringify(data));
+				}).
+			  error(function (err) {
+					console.log("Error: " + err);
+				});
+		};
+
+		$scope.add = function () {
+			if (!$scope.who || !$scope.what || !$scope.when || !$scope.where || !$scope.significance) {
+				return;
+			}
+
+			var who = $scope.who.replace("'", "/");
+			var what = $scope.what.replace("'", "/");
+			var when = $scope.when.toString();
+			var where = $scope.where.replace("'", "/");
+
+			when = when.substr(0, when.length - 15);
+
+			var request = $http({
+				method: "post",
+				url: apiUrl + "addItem",
+				data: {
+					tableName: "links",
+					who:who,
+					what:what,
+					when:when,
+					where:where,
+					ranking:$scope.significance
+				}
+			});
+
+			request.success(function (data) {
+				console.log(data);
+			});
+
+			$http.post(apiUrl + "cool").
+				success(function (data) {
+					console.log(data);
+				}).
+				error(function (data) {
+					console.log(data);
+				});
+		};
 
 		$scope.goto = function (where) {
 			$location.path('/' + where);
