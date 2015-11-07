26b776174bb3d5599d7831d06e416a5dbf41b6a0
Michael Thornbrugh <mpthornbrugh@gmail.com>
2015-11-07 14:49:59 -0600

    bubble view is now connected to the service.

--- bubbles/app.js
+++ bubbles/app.js
@@ -3,7 +3,8 @@
 // Declare app level module which depends on views, and components
 angular.module('bubbleView', [
 	'ngRoute',
-	'bubbleView.view'
+	'bubbleView.view',
+	'databaseEntry.service'
 ])
 	.config(['$routeProvider', function ($routeProvider) {
 		$routeProvider.otherwise({redirectTo: '/view'});
